d352e7075ff364c7c0debed0875a9370ae8435c5
pakeeza21 <162461284+pakeeza21@users.noreply.github.com>
2024-05-26 13:38:49 +0500

    Create _var.js

---
+++ src/components/utils/_var.js
@@ -0,0 +1,16 @@
+const fonts = {
+  $titleFont: "Ubuntu, sans-serif",
+  $mainFont: "Raleway, sans-serif",
+  $latoFont: "Lato, sans-serif",
+};
+
+const colors = {
+  $colorBg: "#fff",
+  $colorCardBg: "#292a73", //"#302f34",
+  $colorGold: "#292a73",
+  $black: "#000",
+  $darkBlue: "#1a1b4b",
+  $orange: "#f7a400",
+};
+
+export { fonts, colors };
